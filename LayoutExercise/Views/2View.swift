33bda9a2813d5eb9d445fc3f8c1478c62db72d9e
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2021-10-26 14:48:50 -0400

    Finished first, created second.

---
+++ LayoutExercise/Views/2View.swift
@@ -0,0 +1,20 @@
+//
+//  2View.swift
+//  LayoutExercise
+//
+//  Created by Jacobo de Juan Millon on 2021-10-26.
+//
+
+import SwiftUI
+
+struct _View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct _View_Previews: PreviewProvider {
+    static var previews: some View {
+        _View()
+    }
+}
--- LayoutExercise/Views/ContentView.swift
+++ LayoutExercise/Views/ContentView.swift
@@ -9,8 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            VStack {
+                AsyncImage(url: URL(string:
+                                        "http://placekitten.com/125/200"))
+                    .frame(width: 125, height: 200)
+                
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.")
+            }
+            VStack {
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+                    
+                
+                AsyncImage(url: URL(string:
+                                        "http://placekitten.com/125/200"))
+                    .frame(width: 125, height: 200)
+                
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.")
+            }
+            VStack {
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.")
+                AsyncImage(url: URL(string:
+                                        "http://placekitten.com/125/200"))
+                    .frame(width: 125, height: 200)
+            }
+        }
     }
 }
 
