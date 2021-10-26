4af78a86fb1d11dd20c5b7ffce35da830d8d4b89
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2021-10-26 15:03:38 -0400

    Started the second view.

--- LayoutExercise/Views/2View.swift
+++ LayoutExercise/Views/2View.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct _View: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack (alignment: .bottom){
+                Text("Heading goes here.")
+                AsyncImage(url: URL(string:
+                    "http://placekitten.com/125/200"))
+                    .frame(width: 125, height: 200)
+                AsyncImage(url: URL(string:
+                    "http://placekitten.com/125/200"))
+                    .frame(width: 125, height: 200)
+            }
+            HStack {
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.")
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.")
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.")
+                VStack {
+                    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.")
+                    HStack(alignment: .bottom) {
+                        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.")
+                        AsyncImage(url: URL(string:
+                            "http://placekitten.com/125/200"))
+                            .frame(width: 125, height: 200)
+                    }
+                }
+            }
+        }
     }
 }
 
