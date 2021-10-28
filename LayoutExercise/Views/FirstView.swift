dff232e261a4c74d3593df57d64f6be915d47c09
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2021-10-27 23:11:19 -0400

    Final touches

--- LayoutExercise/Views/FirstView.swift
+++ LayoutExercise/Views/FirstView.swift
@@ -12,8 +12,8 @@ struct FirstView: View {
         HStack {
             VStack {
                 AsyncImage(url: URL(string:
-                                        "http://placekitten.com/125/200"))
-                    .frame(width: 125, height: 200)
+                                        "http://placekitten.com/100/200"))
+                    .frame(width: 100, height: 200)
                 
                 Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.")
             }
@@ -22,16 +22,16 @@ struct FirstView: View {
                 
                 
                 AsyncImage(url: URL(string:
-                                        "http://placekitten.com/125/200"))
-                    .frame(width: 125, height: 200)
+                                        "http://placekitten.com/100/200"))
+                    .frame(width: 100, height: 200)
                 
                 Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.")
             }
             VStack {
                 Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.")
                 AsyncImage(url: URL(string:
-                                        "http://placekitten.com/125/200"))
-                    .frame(width: 125, height: 200)
+                                        "http://placekitten.com/100/200"))
+                    .frame(width: 100, height: 200)
             }
         }
     }
